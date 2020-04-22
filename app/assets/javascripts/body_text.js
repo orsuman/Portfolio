b97e3587fb3a55c87cfd8c7cb3540166d42edb7a
黒田健太 <96ken@kurodakentanoMacBook-Air.local>
2020-04-22 12:14:33 +0900

    [Fix]ビュー・内容修正

--- app/assets/javascripts/body_text.js
+++ app/assets/javascripts/body_text.js
@@ -1,6 +1,6 @@
 $(function(){
  $(".body-text").on("keyup", function() {
    let countNum = String($(this).val().length);
-   $(".text-count").text("現在の文字数：" + countNum + "文字");
+   $(".text-count").text("現在の文字入力数：" + countNum + "文字");
  });
 });
