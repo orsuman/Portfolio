61f88d3de2ee4a63f14dd1f544d46b291eb40b17
黒田健太 <96ken@kurodakentanoMacBook-Air.local>
2020-04-25 15:15:05 +0900

    [add]画像保存設定

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: "AWS_ACCESS_KEY_ID",
-  secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  region: "AWS_Region",
-  bucket: "AWS_Bucket",
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_Region'],
+  bucket: ENV['AWS_Bucket'],
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
